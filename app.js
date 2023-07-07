2712a8ff85646956f0d1d9fe5c570141e9111be3
oatando <fernandoparrarengifo20@gmail.com>
2023-07-07 09:14:05 -0500

    ocultando archvo

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ app = express()
 
 app.set('port', process.env.PORT || 4000)
 
+// app.get('/', (req, res)=>{
+//     res.send("welcome ")
+// })
+
 
 app.listen(app.get('port'), () => {
     console.log('corriendo en el puerto', app.get('port'))
